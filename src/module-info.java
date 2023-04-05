1cd89f79242fbcc30943a217042b179a36cc5671
horemalvarez <horemalvarez@192.168.1.26>
2023-04-05 19:45:17 +0200

    PRÁCTICA UF2 sin cambios.

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author horemalvarez
+ *
+ */
+module practica1 {
+}
---
+++ src/practica1/notas.java
@@ -0,0 +1,139 @@
+package practica1;
+import java.util.Scanner;
+
+/**
+ * Tenemos una clase llamada Notas con varios métodos dentro que nos darán opción a  ingresar notas, calcular su promedio y definir si es aprobado o no.
+ * @author Horem Álvarez
+ */
+public class notas {
+	
+	//declaramos la variables que nos hacen falta
+		double uf1, uf2, uf3;
+		double acu1, acu2, acu3, def;
+		//utilizamos Scanner para poder introducir datos
+		Scanner entrada = new Scanner(System.in);
+		
+		///vamos ca crear un metodo para ingresar 
+		
+		/**
+		 * Método IngresaNotas se pide al usuario que ingrese las notas del estudiante.
+		 */
+		public void IngresaNotas() {
+			//cuando ejeuctamos este metodo lo primnero que queremos es que nos pida notas
+			System.out.println("ingrese las notas del estudiante");
+			
+			System.out.print("ingrese nota 1: ");
+			
+			uf1= entrada.nextDouble();
+
+			System.out.print("ingrese nota 2: ");
+			
+			uf2= entrada.nextDouble();
+			System.out.print("ingrese nota 3: ");
+			
+			uf3= entrada.nextDouble();
+			
+		
+		}
+		
+		/**
+		 * Método Comprobacion aquí se comprueba si las notas ingresadas están dentro de los valores marcados.
+		 */
+		
+		public void comprobarcion(){
+			
+			if (uf1>10) {
+				System.out.println(" nota1 mal introducida");
+				
+			}else {
+				System.out.println(" nota1 correcta");
+			}
+			
+			if (uf2>10) {
+				System.out.println(" nota2 mal introducida");
+				
+			}else {
+				System.out.println(" nota2 correcta");
+			}
+			if (uf3>10) {
+				System.out.println(" nota3 mal introducida");
+				
+			}else {
+				System.out.println(" nota3 correcta");
+			}
+			
+				
+			
+			
+		}
+		
+		/**
+		 * Método CalculoNotas calcula haciendo el  promedio de las notas ingresadas.
+		 */
+		
+		public void Calculonotas() {
+			acu1= uf1*0.35;
+			acu2 = uf2 * 0.35;
+			acu3 = uf3 * 0.30;
+			
+			def = acu1 + acu2+ acu3;
+			
+			//hasta aqui la tenemos calculada peor no la mostramos
+		}
+		
+		/**
+		 * Método Mostrar nos enseña las notas ingresadas y el promedio del cálculo de notas.
+		 */
+		public void Mostrar() {
+			//print ln lo que hace es que al terminar el print baja la linea
+			System.out.println(" notas introducidas son:");
+			System.out.println(" nota1 = " + uf1);
+			System.out.println(" nota2 = " + uf2);
+			System.out.println(" nota3 = " + uf3);
+			
+			System.out.println(" acumuado 1 = "+ acu1);
+			System.out.println(" acumuado 2 = "+ acu2);
+			System.out.println(" acumuado 3 = "+ acu3);
+			
+			System.out.println(" nota definitiva es = "+ def);
+			
+		}
+		
+		/**
+		 * Método Aprobado nos dirá si se ha aprobado o suspendido.
+		 */
+
+		public void aprobado() {
+				
+				if(def<5 && def>=0) {
+					System.out.println("suspendio");
+				}else {
+					if (def>=5 && def<=10 ) {
+					System.out.println("aprobado");
+					}else {
+						System.out.println(" error en la notas");
+					}
+				}
+			}
+
+	public static void main(String[] args) {
+		
+		// creamos mecanimos para llamar a cualquier metodo fuero de la clase
+				notas fc= new notas();
+				
+				fc.IngresaNotas();
+				
+				fc.comprobarcion();
+				
+
+				fc.Calculonotas();
+				
+				fc.Mostrar();
+				
+				fc.aprobado();
+				
+			}
+
+		}
+
+
